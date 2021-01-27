a3a0ff1ac3c412b7dc62ee1d281fb95ff2567065
Trumpy <bltrumpy@avistainc.com>
2021-01-27 10:35:47 -0600

    Add color support to logger interface

--- source/Logger.cpp
+++ source/Logger.cpp
@@ -6,3 +6,11 @@ void Logger::Log(std::string string)
 {
 	std::cout << string << std::endl;
 }
+
+void Logger::Log(std::string string, ColorSupport::TextColor color)
+{
+	col.GetColorStream(color) << string;
+
+	// Write a blank string with default color to reset the color. Prevents color bleed.
+	col.GetColorStream(col.white) << "" << std::endl;
+}
